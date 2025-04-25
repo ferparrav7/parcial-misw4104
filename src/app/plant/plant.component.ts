989bf3eb4bda6fa75b33988065e2a7f7f2536297
Fernando Parra <f.parrav@uniandes.edu.co>
2025-04-25 12:59:27 -0500

    Crea modulo y componente plant

---
+++ src/app/plant/plant.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-plant',
+    standalone: false,
+    templateUrl: './plant.component.html',
+    styleUrls: ['./plant.component.css']
+})
+export class PlantComponent {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
---
+++ src/app/plant/plant.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {PlantComponent} from './plant.component';
+
+
+@NgModule({
+    declarations: [
+        PlantComponent
+    ],
+    exports: [
+        PlantComponent
+    ],
+    imports: [
+        CommonModule
+    ]
+})
+export class PlantModule {
+}
