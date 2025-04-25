973312c650f32cd032b20d77c5deba466373f1ac
Fernando Parra <f.parrav@uniandes.edu.co>
2025-04-25 14:52:19 -0500

    Crea vista básica para App Vivero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {PlantModule} from './plant/plant.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import {PlantModule} from './plant/plant.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     PlantModule
   ],
